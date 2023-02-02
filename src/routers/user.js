c7249098ffd743bb64b7345494cb19ec72dac4bc
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-02-02 10:27:11 +0100

    V2

--- src/routers/user.js
+++ src/routers/user.js
@@ -51,28 +51,6 @@ router.patch('/updateuser/:id', async (req, res) => {
     }
 });
 
-router.patch('/deleteuser/:id', async (req, res) => {
-    const updates = Object.keys(req.body);
-    const allowedUpdates = ['name', 'email', 'tel', 'age'];
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' });
-    }
-
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-
-        if (!user) {
-            return res.status(404).send({ error: 'No user found!' });
-        }
-
-        res.send(user);
-    } catch (e) {
-        res.status(400).send(e);
-    }
-});
-
 router.delete('/deleteuser/:id', async (req, res) => {
     try {
         const user = await User.findByIdAndDelete(req.params.id);
