fa99741f59232b2284947c268b9d5764a136fb1f
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-02-14 12:36:48 +0100

    V8

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 require('./db/mongoose');
 const userRouter = require('./routers/user');
 const cartRouter = require('./routers/cart');
@@ -7,6 +8,7 @@ const courseRouter = require('./routers/course');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 app.use(cartRouter);
